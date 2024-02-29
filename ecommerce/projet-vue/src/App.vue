8dabc9e84c1551f39bf53b6e2098df14c2884f4a
Marcos Sanches <marcossanches@marcoss-air.wireless.concordia.ca>
2024-02-29 16:56:14 -0500

    Class 4

---
+++ ecommerce/projet-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HeroHeader/>
+    <MainFooter/>
+  </div>
+</template>
+
+<script>
+import HeroHeader from './components/HeroHeader.vue'
+import MainFooter from './components/MainFooter.vue'
+export default {
+  components: {
+    HeroHeader,
+    MainFooter
+  }
+}
+</script>
